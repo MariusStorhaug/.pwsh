c3098cfb663c10f13514ce68d61179e21cc42a19
Marius <Marius.Storhaug@microsoft.com>
2022-02-25 00:07:48 +0100

    powershell profile and settings

---
+++ Microsoft.VSCode_profile.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme star #alien
+
+if ($env:TERM_PROGRAM -eq 'vscode' ) {
+    Set-PSReadLineOption -PredictionViewStyle ListView
+} else {
+    Set-PSReadLineOption -PredictionViewStyle InlineView
+}
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
+if (Get-Command code-insiders -ErrorAction SilentlyContinue) {
+    Set-Alias -Name code -Value code-insiders
+}
+
+function Clear-GitRepo {
+    (git branch).Trim() | Where-Object { $_ -notmatch 'main|\*' } | ForEach-Object { git branch $_ -d -f }
+}
+
+Save-IPConfig
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # .pwsh
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_powershell_config?view=powershell-7.2
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2
